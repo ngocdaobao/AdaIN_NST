29d6ea416f5d93eca94a996490ee2c5b3a4e39aa
Bao Ngoc Dao <162009876+ngocdaobao@users.noreply.github.com>
2025-11-19 16:48:04 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_workers', type=int, default=4)
     parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--shuffle', type=bool, default=True)
+    parser.add_argument('--load_path', type=string, default='None')
 
     args = parser.parse_args()
     epoch = args.epoch
@@ -26,13 +27,14 @@ if __name__ == "__main__":
     num_workers = args.num_workers
     shuffle = args.shuffle
     seed = args.seed
+    load_path = args.load_path
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     style_loader, content_loader = data_loader(batch_size=batch_size, num_workers=num_workers, seed=seed, shuffle=shuffle)
 
     if pretrained_encoder == 'vgg19':
         encoder_model = VGG19Encoder()
-    model = StyleTransferModel(encoder_model=encoder_model).to(device)
+    model = StyleTransferModel(encoder_model=encoder_model, load_path=load_path).to(device)
     print(f'Number of model parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}')
     optimizer = optim.Adam(model.parameters(), lr=lr)
     loss_item, all_loss = trainer(model, style_loader, content_loader, optimizer, device, num_epochs=epoch)
@@ -41,6 +43,7 @@ if __name__ == "__main__":
     # Inference
     # inference(model=model, content_path='AdaIN_NST/example/content.jpg', style_path='AdaIN_NST/example/style.jpg', device=device)
     # print("Inference finished.")
+
 
 
 
